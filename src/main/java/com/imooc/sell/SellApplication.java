ce79e759b6bccc00aeb4a746f241ecb0b1d99de7
panshao <panshao96@hotmail.com>
2019-12-07 18:53:25 +0800

    fix:修复当MySQL版本大于5.6.6后,timestamp为null时不自动设置current timestamp

--- src/main/java/com/imooc/sell/SellApplication.java
+++ src/main/java/com/imooc/sell/SellApplication.java
@@ -2,11 +2,13 @@ package com.imooc.sell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * @author panshao
  */
 @SpringBootApplication
+@EnableJpaAuditing
 public class SellApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/imooc/sell/dataobject/OrderMaster.java
+++ src/main/java/com/imooc/sell/dataobject/OrderMaster.java
@@ -4,7 +4,12 @@ import com.imooc.sell.enums.OrderStatusEnum;
 import com.imooc.sell.enums.PayStatusEnum;
 import lombok.Data;
 import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Date;
@@ -15,6 +20,7 @@ import java.util.Date;
 @Data
 @Entity
 @DynamicUpdate
+@EntityListeners(AuditingEntityListener.class)
 public class OrderMaster {
 
     /** 订单id. */
@@ -42,9 +48,11 @@ public class OrderMaster {
     /** 支付状态, 默认为0未支付. */
     private Integer payStatus = PayStatusEnum.WAIT.getCode();
 
-//    /** 创建时间. */
-//    private Date createTime;
-//
-//    /** 更新时间. */
-//    private Date updateTime;
+    /** 创建时间. */
+    @CreatedDate
+    private Date createTime;
+
+    /** 更新时间. */
+    @LastModifiedDate
+    private Date updateTime;
 }

7501f3082f2c5dcc8fd7dfba3fdfcf32b7c11131
panshao <panshao96@hotmail.com>
2019-12-26 01:10:46 +0800

    feat:增加买家端查询订单及详情，以及取消订单的功能

--- src/main/java/com/imooc/sell/Concroller/BuyerOrderController.java
+++ src/main/java/com/imooc/sell/Concroller/BuyerOrderController.java
@@ -7,17 +7,20 @@ import com.imooc.sell.dto.OrderDTO;
 import com.imooc.sell.enums.ResultEnum;
 import com.imooc.sell.exception.SellException;
 import com.imooc.sell.form.OrderForm;
+import com.imooc.sell.service.BuyerService;
 import com.imooc.sell.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -33,7 +36,11 @@ public class BuyerOrderController {
         //4、取消订单
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private BuyerService buyerService;
 
+
+    //创建订单
     @PostMapping("/create")
     public ResultVO<Map<String, String>> create(@Valid OrderForm orderForm,
                                                 BindingResult bindingResult) {
@@ -52,6 +59,35 @@ public class BuyerOrderController {
         map.put("orderId", createResult.getOrderId());
 
         return ResultVOUtil.success(map);
+    }
+
+    //获取订单列表
+    @GetMapping("/list")
+    public ResultVO<List<OrderDTO>> listResultVO(@RequestParam("openid") String openid,
+                                                 @RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                 @RequestParam(value = "size", defaultValue = "10") Integer size) {
+        if (StringUtils.isEmpty(openid)) {
+            log.error(" 【查询订单列表】 openid为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        PageRequest request = PageRequest.of(page, size);
+        Page<OrderDTO> orderDTOPage = orderService.findList(openid, request);
+        return ResultVOUtil.success(orderDTOPage.getContent());
+    }
+
+    //获取订单详情
+    @GetMapping("/detail")
+    public ResultVO<OrderDTO> detail(@RequestParam("openid") String openid,
+                                     @RequestParam("orderId") String orderId) {
+        return ResultVOUtil.success(buyerService.findOrderOne(openid, orderId));
+    }
+
+    //取消订单
+    @PostMapping("/cancel")
+    public ResultVO cancel(@RequestParam("openid") String openid,
+                           @RequestParam("orderId") String orderId) {
+        buyerService.cancelOrder(openid, orderId);
+        return ResultVOUtil.success();
     }
 
 

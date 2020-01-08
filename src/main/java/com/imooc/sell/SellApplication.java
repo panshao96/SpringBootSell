a2f117fbc02a8023b04aac9bd6f3f33550c00030
panshao <panshao96@hotmail.com>
2020-01-08 20:04:04 +0800

    feat:增加使用mybatis注解方式添加一条productCategory表的功能

--- src/main/java/com/imooc/sell/SellApplication.java
+++ src/main/java/com/imooc/sell/SellApplication.java
@@ -1,5 +1,6 @@
 package com.imooc.sell;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @SpringBootApplication
 @EnableJpaAuditing
+@MapperScan(value = "com.imooc.sell.dataobject.mapper")
 public class SellApplication {
 
     public static void main(String[] args) {
---
+++ src/test/java/com/imooc/sell/dataobject/mapper/ProductCategoryMapperTest.java
@@ -0,0 +1,32 @@
+package com.imooc.sell.dataobject.mapper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
+public class ProductCategoryMapperTest {
+
+    @Autowired
+    private ProductCategoryMapper mapper;
+
+    @Test
+    public void insertByMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("category_name", "mapper新增");
+        map.put("category_type", 105);
+        int result = mapper.insertByMap(map);
+        Assert.assertEquals(1, result);
+    }
+}

70eda24c54e18118bd9d11d8642d3e55c2bd81a2
panshao <panshao96@hotmail.com>
2020-01-08 20:37:27 +0800

    feat:增加使用mybatis注解方式删改查category表的方法

--- src/test/java/com/imooc/sell/dataobject/mapper/ProductCategoryMapperTest.java
+++ src/test/java/com/imooc/sell/dataobject/mapper/ProductCategoryMapperTest.java
@@ -1,5 +1,6 @@
 package com.imooc.sell.dataobject.mapper;
 
+import com.imooc.sell.dataobject.ProductCategory;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.*;
@@ -28,5 +30,37 @@ public class ProductCategoryMapperTest {
         map.put("category_type", 105);
         int result = mapper.insertByMap(map);
         Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void insertByObject() {
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setCategoryName("ByObject");
+        productCategory.setCategoryType(103);
+        int result = mapper.insertByObject(productCategory);
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void findByCategoryType() {
+        ProductCategory result = mapper.findByCategoryType(105);
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void findByCategoryName() {
+        List<ProductCategory> result = mapper.findByCategoryName("男生专享");
+        Assert.assertNotEquals(0, result.size());
+    }
+
+    @Test
+    public void updateByCategoryType() {
+        Assert.assertNotEquals(0, mapper.updateByCategoryType("更新", 103));
+    }
+
+    @Test
+    public void deleteByCategoryType() {
+        int result = mapper.deleteByCategoryType(105);
+        Assert.assertNotEquals(0, result);
     }
 }
